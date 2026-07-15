b33d05d5e69471b39133923d8f14f7f3e19be517
Amber <amberjain967@gmail.com>
2026-07-16 00:04:48 +0530

    Add Weather App v1

---
+++ pratcie.py
@@ -0,0 +1,8 @@
+f = open("myfile.txt", "x")
+with open("demofile.txt", "a") as f:
+  f.write("Now the file has more content!")
+  
+
+#open and read the file after the appending:
+with open("demofile.txt") as f:
+  print(f.read())
