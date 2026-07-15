ab8f8cdf91940150fb1091a30d4d5459e640872a
Amber Jain <amberjain967@gmail.com>
2026-07-16 00:12:06 +0530

    Delete pratcie.py

--- pratcie.py
+++
@@ -1,8 +0,0 @@
-f = open("myfile.txt", "x")
-with open("demofile.txt", "a") as f:
-  f.write("Now the file has more content!")
-  
-
-#open and read the file after the appending:
-with open("demofile.txt") as f:
-  print(f.read())
